8c44c435ffc21ae6c5be21ce1c065e1c543a229b
aleksandarp30 <petrushevskia9@gmail.com>
2024-02-09 17:37:56 +0100

    aleksandar commit 1

---
+++ wp-content/themes/carclubmk/front-page.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <?php wp_head(); ?>
+</head>
+<body>
+    
+    <?php if( have_posts()):
+            while( have_posts()):
+             the_post();
+
+            endwhile;
+    
+        endif;
+        ?>
+    <?php 
+    
+        wp_footer();?>
+</body>
+</html>
---
+++ wp-content/themes/carclubmk/functions.php
@@ -0,0 +1,54 @@
+<?php
+
+add_action('hook_name', 'function_name');
+add_filter('filter_name', 'function_name_1');
+
+$name = "Aleksandar";
+$number = 5;
+$number2 = 10;
+$students = array(
+    'name1' => 'Aleksandar',
+    'name2' => 'Ilija',
+    'name3' => 'Ruzica'
+);
+
+$studentNumber = 12262;
+
+if( $number > $number2 ):
+    echo $number . ' is bigger than' . $number2;
+else:
+    echo $number . ' is smaller than ' . $number2;
+
+endif;
+
+echo '<br/><br/>';
+
+foreach( $students as $key => $students ):
+    echo $students . ' ';
+endforeach;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
